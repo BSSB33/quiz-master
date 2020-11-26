400397f9223b4d105eddde871c4d29b12da88a25
pascaltippe <s_tippe@hwr-berlin.de>
2020-11-26 17:09:43 +0100

    REST replace int time by LocalDateTime

--- src/main/java/com/quizmaster/backend/entities/Response.java
+++ src/main/java/com/quizmaster/backend/entities/Response.java
@@ -3,10 +3,12 @@ package com.quizmaster.backend.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class Response {
     int code;
-    int time;
+    LocalDateTime time;
     String gameID;
 }
